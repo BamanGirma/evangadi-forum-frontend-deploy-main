3253c61540ac70086680dbcc18ec8e2d0f8ee743
BamanGirma <amanuelgirma360@gmail.com>
2024-11-01 16:42:59 +0300

    update axios

--- src/axios/axiosConfig.js
+++ src/axios/axiosConfig.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 const port = import.meta.env.VITE_PORT
-
+const baseURL = import.meta.env.MODE === "development" ? "/api" : "https://evangadi-forum-backend-main.onrender.com/api";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: baseURL,
 });
 
 export default axiosInstance;
